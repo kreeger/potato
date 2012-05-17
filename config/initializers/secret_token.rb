abae2055bcb66e3a0325401dc61a5160c14d9161
Benjamin Kreeger <ben@kree.gr>
2012-05-16 20:36:07 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potato::Application.config.secret_token = '9d8fe9ffec9165e66a877f35f4b322455bd4fc6265eba46b5adf39bdf5599eb07095fa2039fc3fa0fb0a4093416683f3aa5110534e7fa92c7185b206ee74b108'
